ff3152aa36dab09625b21320eab1c2bf5b238f6c
Brunao77 <brucolon@gmail.com>
2022-01-06 03:30:38 -0300

    Chakra ui

--- src/services/getVideos.js
+++ src/services/getVideos.js
@@ -4,7 +4,7 @@ dotenv.config()
 export const getVideos = async () => {
 
   try {
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=statistics&part=snippet&chart=mostPopular&key=${process.env.VITE_API_KEY_YOUTUBE}&maxResults=200&regionCode=es`);
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=statistics&part=snippet&chart=mostPopular&key=${import.meta.env.VITE_API_KEY_YOUTUBE}&maxResults=200&regionCode=es`);
 
     if (response.status === 200) {
       return await response.json();
